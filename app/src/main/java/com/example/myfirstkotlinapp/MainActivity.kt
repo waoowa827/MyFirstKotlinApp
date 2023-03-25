087053cf8f03af2a57cc7fbd834e787a9f0db88e
waoowa827 <ashishdubey827@gmail.com>
2023-03-26 01:39:52 +0530

    Push first kotlin fresh code

--- app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.myfirstkotlinapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
